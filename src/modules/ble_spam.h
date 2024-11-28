d193b0ec957128b338c47b7025415fe15cffefc6
KAJPERdev <69474629+KAJPER@users.noreply.github.com>
2024-11-28 15:22:05 +0100

    dodamoe podstawowych funkcji i hierarhia plikow

--- src/modules/ble_spam.h
+++ src/modules/ble_spam.h
@@ -5,6 +5,5 @@
 #include <NimBLEUtils.h>
 #include <NimBLEServer.h>
 #include <esp_gap_ble_api.h>
-#include <U8g2lib.h>
 void aj_adv(int ble_choice);
 
---
+++ src/modules/display.h
@@ -0,0 +1,2 @@
+#pragma once
+#include <U8g2lib.h>
